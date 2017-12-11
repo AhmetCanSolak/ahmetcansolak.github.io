d67b92c09144a4d300bc82be93f770d5f41219bc
AhmetCanSolak <asolak14@ku.edu.tr>
2017-12-11 18:18:26 +0300

    welcome card

--- blog/blog.js
+++ blog/blog.js
@@ -25,3 +25,26 @@ $(window).load(function(){
          return false;
     });
 });
+
+var IP;
+var CITY;
+
+$(document).ready(function() {
+  getGeoLocation();
+});
+
+function getGeoLocation() {
+  return $.getJSON('https://freegeoip.net/json/')
+    .done(function(location) {
+      console.log("location found!");
+      //console.log(location.ip);
+      p = document.createElement("p");
+      p.innerHTML =  location.ip;
+      document.getElementById('ip').innerHTML= location.ip;
+      document.getElementById('city').innerHTML= location.city;
+      document.getElementById('country').innerHTML= location.country_name;
+      document.getElementById('latitude').innerHTML= location.latitude;
+      document.getElementById('longitude').innerHTML= location.longitude;
+      return location;
+    });
+}
