994cb3d3e6fc373a2b88bf9515be89deaa368c91
AhmetCanSolak <asolak14@ku.edu.tr>
2018-10-08 23:07:43 -0700

    version update to 1.1.1

---
+++ v1.1.1/js/application.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+
+  $(function(){
+        $(".typed").typed({
+          strings: ["computer vision softwares.", "system administration scripts.", "signal processing tools.", "data science projects.", "lots of stuff, tough this box may go crazy sometimes."],
+          typeSpeed: 1.1,
+          loop: true,
+          backDelay: 1700
+        });
+    });
+
+      // $('a.page-scroll').bind('click', function(event) {
+      //     var $anchor = $(this);
+      //     $('html, body').stop().animate({
+      //         scrollTop:  ($($anchor.attr('href')).offset().top - 50)
+      //     }, 1250, 'easeInOutExpo');
+      //     event.preventDefault();
+      // });
+
+    console.log( "ready!" );
+});
